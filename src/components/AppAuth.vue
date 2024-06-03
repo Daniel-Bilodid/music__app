5742d4f95ea825ff466a455ff86bbfa36de73a62
Daniel <danielbeloded@gmail.com>
2024-06-03 11:38:02 +0300

    added validation for registration popup

--- src/components/AppAuth.vue
+++ src/components/AppAuth.vue
@@ -108,7 +108,6 @@
             <div class="mb-3">
               <label class="inline-block mb-2">Age</label>
               <vee-field
-                type="number"
                 name="age"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
               />
@@ -117,31 +116,38 @@
             <!-- Password -->
             <div class="mb-3">
               <label class="inline-block mb-2">Password</label>
-              <input
+              <vee-field
                 type="password"
+                name="password"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
                 placeholder="Password"
               />
+              <ErrorMessage class="text-red-600" name="password" />
             </div>
             <!-- Confirm Password -->
             <div class="mb-3">
               <label class="inline-block mb-2">Confirm Password</label>
-              <input
+              <vee-field
                 type="password"
+                name="confirm_password"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
                 placeholder="Confirm Password"
               />
+              <ErrorMessage class="text-red-600" name="confirm_password" />
             </div>
             <!-- Country -->
             <div class="mb-3">
               <label class="inline-block mb-2">Country</label>
-              <select
+              <vee-field
+                as="select"
+                name="country"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
               >
                 <option value="USA">USA</option>
                 <option value="Mexico">Mexico</option>
                 <option value="Germany">Germany</option>
-              </select>
+              </vee-field>
+              <ErrorMessage class="text-red-600" name="country" />
             </div>
             <!-- TOS -->
             <div class="mb-3 pl-6">
@@ -173,10 +179,10 @@ export default {
       schema: {
         name: 'required|min:3|max:100|alpha_spaces',
         email: 'required|email',
-        age: 'required|minVal:18|maxVal:100',
-        password: '',
-        confirm_password: '',
-        country: '',
+        age: 'required|min_value:18|max_value:100',
+        password: 'required|min:5|max:100',
+        confirm_password: 'confirmed:@password',
+        country: 'required|',
         tos: ''
       }
     }
