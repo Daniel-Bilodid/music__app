8d7db29fa146b6ec31c0a457629baa3b8f52b697
Даніель Білодід <danielbilodid@MacBook-Air-Daniel.local>
2024-06-05 17:01:15 +0300

    added the custom error messages

--- src/components/AppAuth.vue
+++ src/components/AppAuth.vue
@@ -81,7 +81,12 @@
             </button>
           </form>
           <!-- Registration Form -->
-          <vee-form v-show="tab === 'register'" :validation-schema="schema" @submit="register">
+          <vee-form
+            v-show="tab === 'register'"
+            :validation-schema="schema"
+            @submit="register"
+            :initial-values="userData"
+          >
             <!-- Name -->
             <div class="mb-3">
               <label class="inline-block mb-2">Name</label>
@@ -116,12 +121,16 @@
             <!-- Password -->
             <div class="mb-3">
               <label class="inline-block mb-2">Password</label>
-              <vee-field
-                type="password"
-                name="password"
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-                placeholder="Password"
-              />
+              <vee-field name="password" :bails="false" v-slot="{ field, errors }">
+                <input
+                  type="password"
+                  class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+                  placeholder="Password"
+                  v-bind="field"
+                />
+                <div class="text-red-600" v-for="error in errors" :key="error">{{ error }}</div>
+              </vee-field>
+
               <ErrorMessage class="text-red-600" name="password" />
             </div>
             <!-- Confirm Password -->
@@ -144,6 +153,7 @@
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
               >
                 <option value="USA">USA</option>
+                <option value="Ukraine">Ukraine</option>
                 <option value="Mexico">Mexico</option>
                 <option value="Germany">Germany</option>
                 <option value="Antarctica">Antarctica</option>
@@ -187,10 +197,13 @@ export default {
         name: 'required|min:3|max:100|alpha_spaces',
         email: 'required|email',
         age: 'required|min_value:18|max_value:100',
-        password: 'required|min:5|max:100',
-        confirm_password: 'confirmed:@password',
-        country: 'required|excluded:Antarctica',
-        tos: 'required'
+        password: 'required|min:9|max:100|excluded:password',
+        confirm_password: 'passwords_missmatch:@password',
+        country: 'required|country_excluded:Antarctica',
+        tos: 'tos'
+      },
+      userData: {
+        country: 'Ukraine'
       }
     }
   },
