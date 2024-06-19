a81187acbddab170d2c876520c10fbbb63035df6
Daniel <danielbeloded@gmail.com>
2024-06-19 18:00:14 +0300

    added the routing to the project

--- src/components/AppRegisterForm.vue
+++ src/components/AppRegisterForm.vue
@@ -81,6 +81,22 @@
       </vee-field>
       <ErrorMessage class="text-red-600" name="country" />
     </div>
+
+    <!-- Artist of Listener -->
+
+    <div class="mb-3">
+      <label class="inline-block mb-2">Who are you?</label>
+
+      <vee-field
+        as="select"
+        name="activity"
+        class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+      >
+        <option value="Listener">Listener</option>
+        <option value="Artist">Artist</option>
+      </vee-field>
+      <ErrorMessage class="text-red-600" name="activity" />
+    </div>
     <!-- TOS -->
     <div class="mb-3 pl-6">
       <vee-field
@@ -103,7 +119,8 @@
 </template>
 
 <script>
-import { auth } from '@/includes/firebase'
+import { mapActions } from 'pinia'
+import useUserStore from '@/stores/user'
 
 export default {
   name: 'AppRegisterForm',
@@ -117,6 +134,7 @@ export default {
         password: 'required|min:9|max:100|excluded:password',
         confirm_password: 'passwords_missmatch:@password',
         country: 'required|country_excluded:Antarctica',
+        activity: 'required',
         tos: 'tos'
       },
 
@@ -129,21 +147,20 @@ export default {
       reg_alert_msg: 'Please wait! Your account is being created.'
     }
   },
+
   methods: {
+    ...mapActions(useUserStore, {
+      createUser: 'register'
+    }),
     async register(values) {
       this.reg_show_alert = true
       this.reg_in_submission = true
       this.reg_alert_variant = 'bg-blue-500'
       this.reg_alert_msg = 'Please wait! Your account is being created.'
 
-      let userCred = null
-
       try {
-        userCred = await auth.createUserWithEmailAndPassword(values.email, values.password)
-
-        console.log(userCred)
+        await this.createUser(values)
       } catch (error) {
-        console.log(userCred)
         this.reg_in_submission = false
         this.reg_alert_variant = 'bg-red-500'
         this.reg_alert_msg = 'An unexpected error occured. Please try again later.'
@@ -152,7 +169,7 @@ export default {
 
       this.reg_alert_variant = 'bg-green-500'
       this.reg_alert_msg = 'Success! Your account has been created.'
-      console.log(userCred)
+      window.location.reload()
     }
   }
 }
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
+import Home from '@/views/AppHome.vue'
+import AppAbout from '@/views/AppAbout.vue'
+import AppManage from '@/views/AppManage.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: []
+  routes: [
+    {
+      path: '/',
+      component: Home
+    },
+    {
+      path: '/about',
+      component: AppAbout
+    },
+    {
+      path: '/manage',
+      component: AppManage
+    }
+  ]
 })
 
 export default router
