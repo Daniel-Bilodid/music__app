95116becf109bd1c9f95315169c84bc3ce0649ea
Даніель Білодід <danielbilodid@MacBook-Air-Daniel.local>
2024-06-25 13:39:15 +0300

    added some privacy fixes

--- src/includes/firebase.js
+++ src/includes/firebase.js
@@ -1,20 +1,23 @@
 import firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/firestore'
+import 'firebase/storage'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBqp5eRc-fCouMSfCxaxFpwk2uIKc9g4fk',
-  authDomain: 'music-eb088.firebaseapp.com',
-  projectId: 'music-eb088',
-  storageBucket: 'music-eb088.appspot.com',
-  appId: '1:690155803799:web:c0e99f9f05262687db45d2'
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 }
 
 firebase.initializeApp(firebaseConfig)
 
 const auth = firebase.auth()
 const db = firebase.firestore()
+const storage = firebase.storage()
 
 const usersCollection = db.collection('users')
+const songsCollection = db.collection('songs')
 
-export { auth, db, usersCollection }
+export { auth, db, usersCollection, songsCollection, storage }
