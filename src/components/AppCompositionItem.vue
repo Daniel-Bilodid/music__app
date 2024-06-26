13c67fd3c26539f2cf4732f60b386c3263ee048e
Daniel <danielbeloded@gmail.com>
2024-06-26 17:56:32 +0300

    added song validation

--- src/components/AppCompositionItem.vue
+++ src/components/AppCompositionItem.vue
@@ -13,15 +13,23 @@
       </button>
     </div>
     <div v-show="showForm">
-      <vee-form :validation-schema="titleSchema" @submit="changeMusic">
+      <div
+        class="text-white text-center font-bold p-4 mb-4"
+        v-if="show_alert"
+        :class="alert_vatiant"
+      >
+        {{ alert_message }}
+      </div>
+      <vee-form :initial-values="song" :validation-schema="schema" @submit="edit">
         <div class="mb-3">
           <label class="inline-block mb-2">Song Title</label>
           <vee-field
-            name="name"
+            name="modified_name"
             type="text"
             class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
             placeholder="Enter Song Title"
           />
+          <ErrorMessage class="text-red-600" name="modified_name" />
         </div>
         <div class="mb-3">
           <label class="inline-block mb-2">Genre</label>
@@ -31,15 +39,31 @@
             class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
             placeholder="Enter Genre"
           />
+          <ErrorMessage class="text-red-600" name="genre" />
         </div>
-        <button type="submit" class="py-1.5 px-3 rounded text-white bg-green-600">Submit</button>
-        <button type="button" class="py-1.5 px-3 rounded text-white bg-gray-600">Go Back</button>
+        <button
+          type="submit"
+          class="py-1.5 px-3 rounded text-white bg-green-600"
+          :disabled="in_submission"
+        >
+          Submit
+        </button>
+        <button
+          type="button"
+          class="py-1.5 px-3 rounded text-white bg-gray-600"
+          :disabled="in_submission"
+          @click.prevent="showForm = false"
+        >
+          Go Back
+        </button>
       </vee-form>
     </div>
   </div>
 </template>
 
 <script>
+// import { ErrorMessage } from 'vee-validate'
+import { songsCollection } from '@/includes/firebase'
 export default {
   name: 'AppCompositionItem',
   props: {
@@ -52,14 +76,31 @@ export default {
     return {
       showForm: false,
 
-      titleSchema: {
-        name: 'required|min:3|max:25|alpha_spaces',
-        genre: 'required|min:3|max:25|alpha_spaces'
-      }
+      schema: {
+        modified_name: 'required',
+        genre: 'alpha_spaces'
+      },
+      in_submission: false,
+      show_alert: false,
+      alert_variant: 'bg-blue-500',
+      alert_message: 'Please wait! Updating song info.'
     }
   },
-  async changeMusic(values) {
-    console.log(values)
+  methods: {
+    async edit(values) {
+      this.in_submission = true
+      this.show_alert = true
+      this.alert_variant = 'bg-blue-500'
+      this.alert_message = 'Please wait! Updating song info.'
+
+      try {
+        await songsCollection.doc(this.song.docID).update(values)
+      } catch (error) {
+        this.in_submission = false
+        this.alert_variant = 'bg-red-500'
+        this.alert_message = 'Something went wrong! Try again later'
+      }
+    }
   }
 }
 </script>
