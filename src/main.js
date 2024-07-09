7bac380f25aa2118267d1f654037cfa94da74c98
Даніель Білодід <danielbilodid@MacBook-Air-Daniel.local>
2024-07-09 18:15:00 +0300

    added directeves to the icons

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ import App from './App.vue'
 import router from './router'
 import VeeValidatePlugin from './includes/validation'
 import { auth } from './includes/firebase'
-
+import Icon from './directives/icon'
 let app
 
 auth.onAuthStateChanged(() => {
@@ -18,6 +18,7 @@ auth.onAuthStateChanged(() => {
     app.use(createPinia())
     app.use(router)
     app.use(VeeValidatePlugin)
+    app.directive('icon', Icon)
 
     app.mount('#app')
   }
