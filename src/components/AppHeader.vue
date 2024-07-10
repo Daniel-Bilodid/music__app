e13f4013ec4dd5a58bc8609dea2184467d90b154
Daniel <danielbeloded@gmail.com>
2024-07-10 17:45:22 +0300

    started to adding the website translation

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -30,6 +30,13 @@
             </li>
           </template>
         </ul>
+        <ul class="ml-auto">
+          <li>
+            <a class="px-2 text-white" href="#" @click.prevent="changeLocale">{{
+              currentLocale
+            }}</a>
+          </li>
+        </ul>
       </div>
     </nav>
   </header>
@@ -43,7 +50,10 @@ import useUserStore from '@/stores/user'
 export default {
   name: 'AppHeader',
   computed: {
-    ...mapStores(useModalStore, useUserStore)
+    ...mapStores(useModalStore, useUserStore),
+    currentLocale() {
+      return this.$i18n.locale === 'uk' ? 'Ukranian' : 'English'
+    }
   },
   methods: {
     toggleAuthModal() {
@@ -55,6 +65,9 @@ export default {
       if (this.$route.meta.requiersAuth) {
         this.$router.push({ name: 'home' })
       }
+    },
+    changeLocale() {
+      this.$i18n.locale = this.$i18n.locale === 'uk' ? 'en' : 'uk'
     }
   }
 }
