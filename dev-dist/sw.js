56c8878f1bbefea482a790e109a978631b7f6540
Daniel <danielbeloded@gmail.com>
2024-07-26 19:25:13 +0300

    added progress bar on page loading

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.msq0pjkn13"
+    "revision": "0.o2mpp0h7rr"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
